1e9b6a26e393b0e8655ec5dab0b3d3f501dd14f0
王少魁 <wangshaokui@ssit-xm.com.cn>
2019-02-21 11:18:10 +0800

    项目初始化

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+//优化Css代码，处理浏览器兼容问题
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+    plugins: [
+        autoprefixer()
+    ]
+}
---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="test">
+        {{text}}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            text: "abcd"
+        }
+    }
+}
+</script>
+
+<style>
+    #test {
+        color: red;
+    }
+</style>
